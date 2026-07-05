298d5db35e35f66b5f93418b743e0ca7dce2bad7
Gabriel Braga <gabrielsouza1170@gmail.com>
2026-07-05 02:17:07 -0300

    fix: add @Transactional to markAllUnusedByUserIdAsUsed repository method

--- src/main/java/com/piggy/piggyfinance/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/piggy/piggyfinance/repository/PasswordResetTokenRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -13,6 +14,7 @@ public interface PasswordResetTokenRepository extends JpaRepository<PasswordRese
 
     Optional<PasswordResetToken> findByToken(String token);
 
+    @Transactional
     @Modifying
     @Query("UPDATE PasswordResetToken t SET t.used = true WHERE t.user.id = :userId AND t.used = false")
     void markAllUnusedByUserIdAsUsed(@Param("userId") UUID userId);
